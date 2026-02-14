bae8228fdc81caa3776b1b8ed0a82e5e66d5d400
atancredi <a.tancredi00@gmail.com>
2026-02-14 16:56:54 +0100

    resim.py: utility for plotting resimulated spectra (from test or generated geometries)

--- resim.py
+++ resim.py
@@ -6,14 +6,10 @@ from utils.helper import plot_comsol, read_comsol_csv
 from utils.dataloader import load_reflection_spectra_data
 
 
-if __name__ == "__main__":
-
-    _, _, _, _, wavelength, scaler_geo = load_reflection_spectra_data("__data/pisa_data_2025_12_02.h5")
-
-
-    df_resim = read_comsol_csv("__resimulations/2026_02_10__Alessandro_resim.csv")
-    columns = ['h_pill (um)', 'sep (um)', 'd_pill (um)', 'w_pill (um)', 'freq (THz)', 'Reflectance (1)']
-
+def get_spectra_from_comsol_file(file_path, columns, n_spectra=9):
+    # get resimulated spectra from COMSOL file
+    df_resim = read_comsol_csv(file_path)
+    
     def get_rows(n=1, start=0):
         rows = []
         for i in range(start,n):
@@ -22,66 +18,101 @@ if __name__ == "__main__":
     
 
     # extract spectra
-    spectra = []
-    n_spectra = 9
+    resimulated_spectra = []
     for i in range(n_spectra):
-        spectra.append(get_rows(n=81*(i+1), start=81*i))
+        resimulated_spectra.append(get_rows(n=81*(i+1), start=81*i))
+    
+    return resimulated_spectra
 
 
-    # get target spectra
-    df_target = pd.read_csv("__models/official/version001/generated/test_spectra_001_v3_04_80.csv", header=None)
-    df_geo_target = pd.read_csv("__models/official/version001/generated/test_geometries.csv", header=None)
+test_resimulated = {
+    "resimulated_spectra": "__resimulations/2026_02_10__Alessandro_resim.csv",
+    "target_spectra": "__models/official/version001/generated/test_spectra_001_v3_04_80.csv",
+    "predicted_spectra": "__models/official/version001/generated/pred_spectra_001_v3_04_80_test.csv",
+    "predicted_geometries": "__models/official/version001/generated/geo_params_001_v3_04_80_test.csv"
+}
+
+generated_resimulated = {
+    "resimulated_spectra": "__resimulations/2026_02_10__Alessandro_resim.csv",
+    "target_spectra": "__models/official/version001/generated_custompeaks/test_spectra_001_v3_04_80_custompeaks.csv",
+    "predicted_spectra": "__models/official/version001/generated_custompeaks/pred_spectra_001_v3_04_80_custompeaks.csv",
+    "predicted_geometries": "__models/official/version001/generated_custompeaks/geo_params_001_v3_04_80_custompeaks.csv"
+}
+
+
+
+def plot_resimulated_spectrum(source: str, spectrum_index: int, out_file = "resim.png"):
+    
+    if source == "g": #generated
+        params = generated_resimulated
+        type_str = "suggested"
+    elif source == "t": # test
+        params = test_resimulated
+        type_str = "test dataset"
+    else:
+        raise ValueError("source must be g,t")
+    
+    _, _, _, _, wavelength, scaler_geo = load_reflection_spectra_data("__data/pisa_data_2025_12_02.h5")
+
+    def get_geo_str(geo):
+        geo_transformed = scaler_geo.inverse_transform(geo.reshape(1, -1))[0]
+        return " ".join([f"{i:.3f}" for i in geo_transformed])
+
+    columns = ['h_pill (um)', 'sep (um)', 'd_pill (um)', 'w_pill (um)', 'freq (THz)', 'Reflectance (1)']
+    resimulated_spectra = get_spectra_from_comsol_file(params["resimulated_spectra"], columns, n_spectra=9)
+
+
+    # get target spectra and geometries
+    df_target = pd.read_csv(params["target_spectra"], header=None)
 
     # get predicted spectra
-    df_pred = pd.read_csv("__models/official/version001/generated/pred_spectra_001_v3_04_80_test.csv", header=None)
+    df_pred = pd.read_csv(params["predicted_spectra"], header=None)
 
+    # get predicted geometries
+    #   this is the file sent for the resimulation, so it has the header
+    df_geo_pred = pd.read_csv(params["predicted_geometries"])
 
-
-    spectra_to_plot = [1]
-
-    fig, axes = plt.subplots(1, len(spectra_to_plot), figsize=(12, 6), sharey=True)
-    if len(spectra_to_plot) == 1:
-        axes = [axes]
+    fig, ax = plt.subplots(1, 1, figsize=(12, 6), sharey=True)
     
-    target_np = df_target.to_numpy()
-    geo_target_np = df_geo_target.to_numpy()
-    pred_np = df_pred.to_numpy()
-    for j, spectrum_index in enumerate(spectra_to_plot):
-        
-        df_spectrum = pd.DataFrame(spectra[spectrum_index], columns=columns)
-        target = target_np[spectrum_index]
-        pred = pred_np[spectrum_index]
+    target_np = df_target.to_numpy() # target spectra
+    pred_np = df_pred.to_numpy() # predicted spectra
+    geo_pred_np = df_geo_pred.to_numpy() # predicted geometries
 
+    target = target_np[spectrum_index]
+    pred = pred_np[spectrum_index]
+    pred_geo = geo_pred_np[spectrum_index]
 
-        target_geo = geo_target_np[spectrum_index]
-        target_geo_transformed = scaler_geo.inverse_transform(target_geo.reshape(1, -1))[0]
-        target_geo_str = " ".join([f"{i:.3f}" for i in target_geo_transformed])
-        axes[j].plot(wavelength, target, label = f"Target {target_geo_str}", linestyle='--', color='red')
+    ax.plot(wavelength, target, label = f"Target", linestyle='--', color='red')
 
-        axes[j].plot(wavelength, pred, label = "Predicted", linestyle='--', color='green')
+    pred_geo_str = get_geo_str(pred_geo)
+    ax.plot(wavelength, pred, label = f"Predicted {pred_geo_str}", linestyle='--', color='green')
 
-        plot_comsol(
-            df_spectrum,
-            axes[j],
-            ["Reflectance (1)"], # ["abs(emw.S11)^2 (1)"], #
-            xdata_indx=4,
-            resim=True
-        )
+    # plot resimulated spectrum
+    df_spectrum = pd.DataFrame(resimulated_spectra[spectrum_index], columns=columns)
+    plot_comsol(
+        df_spectrum,
+        ax,
+        ["Reflectance (1)"], # ["abs(emw.S11)^2 (1)"], #
+        xdata_indx=4,
+        resim=True
+    )
 
-        axes[j].set_xlabel("Frequency (THz)")
-        if j == 0:
-            axes[j].set_ylabel("Reflectance")
+    ax.set_xlabel("Frequency (THz)")
+    ax.set_ylabel("Reflectance")
 
-    # axes.set_title("Re-simulation of suggested designs run " + ID)
-    fig.suptitle("Re-simulation of test dataset sample", fontsize=16)
+    fig.suptitle(f"Re-simulation of {type_str} design", fontsize=16)
 
-    axes[0].legend(title="Params. $h_{pill}$, $sep$, $d_{pill}$, $w_{pill}$ all $\mu m$")
-    handles, labels = axes[0].get_legend_handles_labels()
+    ax.legend(title="Params. $h_{pill}$, $sep$, $d_{pill}$, $w_{pill}$ all $\mu m$")
+    handles, labels = ax.get_legend_handles_labels()
     # fig.legend(handles, labels, loc='center right', 
         #    title="Params. $h_{pill}$, $sep$, $d_{pill}$, $w_{pill}$ [$\mu m]$")
 
     # plt.tight_layout(rect=[0, 0.03, 0.8, 0.95])
     plt.tight_layout()
     # plt.show()
-    plt.savefig(f"plots/resim_plotsv2_001_test_id{spectra_to_plot[0]}.png")
-        
+    plt.savefig(out_file)
+
+
+if __name__ == "__main__":
+    from fire import Fire
+    Fire(plot_resimulated_spectrum)
